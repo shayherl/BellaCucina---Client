ea97eb1408599f15c146a79c8ffce0652fba1218
shayherl <shayher@post.bgu.ac.il>
2024-07-09 16:36:40 +0300

    /

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 const state = {
 
-    server_domain: "http://localhost:3000",
+    // server_domain: "http://localhost:3000",
 
-    // server_domain: "http://shay-shahar.cs.bgu.ac.il",
+    server_domain: "http://shay-shahar.cs.bgu.ac.il",
     
 
 };
